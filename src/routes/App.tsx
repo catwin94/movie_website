32e8f33b61fe607aa4f5f20a44e08ceda0f0beb0
catwin94 <cata.carabajal@gmail.com>
2022-01-25 20:22:15 +0100

    trying routing with HashRouter component

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "../screens/Home";
 import Layout from "../screens/Layout";
 import MovieDetails from "../screens/MovieDetails";
@@ -8,7 +8,7 @@ import NotFound from "../screens/NotFound";
 //This component handle the routes of the project
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -16,7 +16,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
