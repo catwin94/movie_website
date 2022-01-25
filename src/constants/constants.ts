fb87fbbbdff3742437d6170284b641ba1ad2c6f7
catwin94 <cata.carabajal@gmail.com>
2022-01-26 00:14:46 +0100

    url for github pages

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
 export const API_KEY = "ae109d42";
-export const BASE_URL = "http://www.omdbapi.com/";
+export const BASE_URL = "https://www.omdbapi.com/";
 export const MOVIE_TYPE_OPTIONS = ["all", "movie", "episode", "series"]; // select filter options
